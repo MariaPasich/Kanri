92f3437569171f3a19ba03c4da292b5444b31d94
Myroslav May <myroslav.m2014@gmail.com>
2026-05-04 11:16:16 +0300

    login + user token start

--- app/components/dashboard/Dashboard.tsx
+++ app/components/dashboard/Dashboard.tsx
@@ -1,4 +1,14 @@
-export function Dashboard() {
+import { useNavigate } from "react-router";
+
+export default function Dashboard() {
+  const navigate = useNavigate()
+  // protected route
+  if (typeof window !== 'undefined') {
+    if (localStorage.getItem("usr_tk") == null) {
+      navigate("/login", { replace: true })
+  }
+  }
+
   return (
     <main>
       <h1>Kanri - Dashboard</h1>
--- app/components/login/Login.tsx
+++ app/components/login/Login.tsx
@@ -1,7 +1,16 @@
-export function Login() {
+export default function Login() {
   return (
     <main>
       <h1>Kanri - Login</h1>
+      <br/>
+      <br/>
+      <label htmlFor="username">Username: </label>
+      <input type="text" name="username" id="username" />
+
+      <label htmlFor="password">Password: </label>
+      <input type="text" name="password" id="password" />
+
+      <button>Submit</button>
     </main>
   );
 }
