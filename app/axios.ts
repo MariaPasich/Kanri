6c159a8328a340789ba18c90f15eefabce5f66cc
Myroslav May <myroslav.m2014@gmail.com>
2026-06-08 21:15:08 +0300

    bump ++

--- app/axios.ts
+++ app/axios.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const axiosFetcher = axios.create({
-    baseURL: "https://NEED_TO_BE_REPLACED.io",
+    baseURL: "https://kanri-backend.onrender.com",
     timeout: 1000,
-    headers: { 'X-JWT-login': "NEEDS_TO_BE_SET" }
+    headers: { 'X-JWT-login': "3073e40290d0cbe6e3c82286f133e1bad00af5693d592f4e6ffd85f11748fa34" }
 })
 
 export default axiosFetcher
